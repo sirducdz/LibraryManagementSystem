3cb2a3e404675288983baedca03016ada3db6d07
NGUYEN VAN DUC <ducnvhe160331@fpt.edu.vn>
2025-04-30 22:39:23 +0700

    import tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,15 @@ import { UseAuth, UseProvider, PI, NewObject } from "./authContextTmp.jsx";
 import { UseAuth1 } from "./authContextTmp.jsx";
 import { App1 } from "./demoTmp.jsx";
 import { App2 } from "./demoTmp2.jsx";
+import Layout from "./components/layout/Layout.jsx";
 createRoot(document.getElementById("root")).render(
   <div>
     <BrowserRouter>
       <UseProvider value={456}>
-        <App1 />
+      <App/>
       </UseProvider>
-      <App2 />
+      {/* <App2 /> */}
+      
     </BrowserRouter>
     {NewObject.name}
   </div>
